59ea23efcf761da0481834b2539d92f5cf82b824
Paul Kilgo <paulkilgo@gmail.com>
2013-07-11 20:29:56 -0400

    add tests for box

--- tests/test_box.py
+++ tests/test_box.py
@@ -12,12 +12,21 @@ INTERSECTS = (
   Ray(Vec3(0, 2, 0), -AXIS_Y),
   Ray(Vec3(0, 0, 2), -AXIS_Z),
 )
+NOT_INTERSECTS = (
+  Ray(Vec3(-3, 3, 0), AXIS_X),
+  Ray(Vec3(0, -3, 3), AXIS_Y),
+  Ray(Vec3(3, 0, -3), AXIS_Z),
+  Ray(Vec3(2, 0, 3), -AXIS_X),
+  Ray(Vec3(0, 2, 2), -AXIS_Y),
+  Ray(Vec3(2, 0, 2), -AXIS_Z),
+)
 UNIT = Box.from_radius(1)
 
 class TestBox(SlowhandTest):
 
-
   def test_intersects(self):
     for ray in INTERSECTS:
-      self.assertEquals(UNIT.intersects(ray), (1, 2))
+      self.assertEqual(UNIT.intersects(ray), (1, 3))
+    for ray in NOT_INTERSECTS:
+      self.assertEqual(UNIT.intersects(ray), None)
       
