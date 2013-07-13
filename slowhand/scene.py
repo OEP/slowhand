9fd607c5b203042d1117c999a85a3335e3df23b7
Paul Kilgo <paulkilgo@gmail.com>
2013-07-13 08:49:29 -0400

    patching

--- slowhand/scene.py
+++ slowhand/scene.py
@@ -3,6 +3,11 @@ from multiprocessing import cpu_count, Pool
 from collections import namedtuple
 from .math import Vec3, Vec4
 
+def patch_generator(patchx, patchy, width, height):
+  for px in range(0, width, patchx):
+    for py in range(0, height, patchy):
+      yield px, py, px + patchx, py + patchy
+
 class RenderData(object):
   def __init__(self, ed=None, ec=None, sd=None, sc=None, sm=[]):
     self.ed = ed
@@ -13,7 +18,7 @@ class RenderData(object):
 
 class Scene(object):
   def __init__(self, camera, data, scatter=5, step=0.1, samples=1,
-      callback=None, box=None, threads=cpu_count()):
+      callback=None, box=None, threads=cpu_count(), patch_size=100):
     self.camera = camera
     self.data = data
     self.scatter = scatter
@@ -22,20 +27,25 @@ class Scene(object):
     self.callback = callback
     self.box = box
     self.threads = threads
+    self.patch_size = patch_size
 
   def render(self, image):
     height, width, depth = image.shape
-    def yield_args():
-      for j in range(height):
-        for i in range(width):
-          yield i, j, width, height
+    def yield_args(x1, y1, x2, y2):
+      for j in range(y1, y2):
+        for i in range(x1, x2):
+          if 0 <= i < width and 0 <= j < height:
+            yield i, j, width, height
+
+    patchinfo = self.patch_size, self.patch_size, width, height
     pool = Pool(self.threads)
-    result = pool.map(self._do_render, yield_args())
+    for x1, y1, x2, y2 in patch_generator(*patchinfo):
+      result = pool.map(self._do_render, yield_args(x1, y1, x2, y2))
 
-    for i, j, light in result:
-      if light:
-        image[j][i] = light[:image.ndim]
-
+      for i, j, light in result:
+        if light:
+          image[j][i] = light[:image.ndim]
+      
   def _do_render(self, args):
     i, j, width, height = args
     ray = self.camera.get_ray(i/width, j/height)
@@ -43,9 +53,6 @@ class Scene(object):
     t1 = self.camera.far
     light = Vec4(0)
     T = 1
-    
-    if self.callback:
-      self.callback(j * width + i, width * height)
 
     if self.box:
       intersect = self.box.intersects(ray)
